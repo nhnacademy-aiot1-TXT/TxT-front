b285c787fd289e9e5cd573c6b700752e73c01a7f
pass0210 <pass0210@gmail.com>
2024-03-28 20:51:59 +0900

    Add: RefreshTokenResponse 추가
    RefreshTokenResponse를 사용하기 위해 dto 추가

---
+++ src/main/java/com/nhnacademy/front/dto/RefreshTokenResponse.java
@@ -0,0 +1,17 @@
+package com.nhnacademy.front.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RefreshTokenResponse {
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("expire_in")
+    private Integer expiresIn;
+}
