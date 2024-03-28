365970cc01b66760c90b8bb8a8e1e3a9280aa9be
pass0210 <pass0210@gmail.com>
2024-03-28 20:53:36 +0900

    Refactor: AccessTokenResponse 수정
    인증 서버가 보내는 응답과 일치하게 변경

--- src/main/java/com/nhnacademy/front/dto/AccessTokenResponse.java
+++ src/main/java/com/nhnacademy/front/dto/AccessTokenResponse.java
@@ -1,12 +1,19 @@
 package com.nhnacademy.front.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AccessTokenResponse {
+    @JsonProperty("access_token")
     private String accessToken;
+    @JsonProperty("token_type")
     private String tokenType;
-    private String expireIn;
+    @JsonProperty("expire_in")
+    private Integer expiresIn;
 }
