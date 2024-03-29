4b4554eb55c9ee00b1fbc1be281bb3a342080f52
jongsikk <xxx_@kakao.com>
2024-03-29 14:50:17 +0900

    Add: TokensResponse 추가

--- src/main/java/com/nhnacademy/front/dto/TokensResponse.java
+++ src/main/java/com/nhnacademy/front/dto/TokensResponse.java
@@ -1,2 +1,15 @@
-package com.nhnacademy.front.dto;public class TokensResponse {
+package com.nhnacademy.front.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokensResponse {
+    private AccessTokenResponse accessToken;
+    private RefreshTokenResponse refreshToken;
 }
