8e15405bfba1f7be5e721f71e64eadc7feba0ea9
jongsikk <xxx_@kakao.com>
2024-03-29 14:51:05 +0900

    Feat: reissue 구현

--- src/main/java/com/nhnacademy/front/advice/ExceptionAdvice.java
+++ src/main/java/com/nhnacademy/front/advice/ExceptionAdvice.java
@@ -1,25 +1,34 @@
 package com.nhnacademy.front.advice;
 
-import com.nhnacademy.front.adaptor.UserAdaptor;
+import com.nhnacademy.front.adaptor.UserAdapter;
+import com.nhnacademy.front.dto.AccessTokenResponse;
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
 
 @ControllerAdvice(basePackages = "com.nhnacademy.front")
 @RequiredArgsConstructor
 public class ExceptionAdvice {
-    private final UserAdaptor userAdaptor;
+    private final UserAdapter userAdapter;
     @ExceptionHandler(FeignException.class)
-    public String feignExceptionHandler(FeignException exception, HttpServletRequest request){
+    public String feignExceptionHandler(FeignException exception, HttpServletRequest request, HttpServletResponse response){
         System.out.println(exception.getMessage());
         if(exception.status() == 400){
             return "redirect:/logout";
         } else if (exception.status() == 401) {
             try {
-//                userAdaptor.reissue();
+                Cookie refreshToken = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("refreshToken")).findFirst().orElse(null);
+
+                AccessTokenResponse accesstokenresponse = userAdapter.reissue(refreshToken.getValue());
+                Cookie accessCookie = new Cookie("accessToken", accesstokenresponse.getAccessToken());
+
+                response.addCookie(accessCookie);
             } catch (FeignException e) {
                 return "redirect:/logout";
             }
