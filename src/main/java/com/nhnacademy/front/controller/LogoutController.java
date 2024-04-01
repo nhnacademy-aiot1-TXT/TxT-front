c15c12c03d99c3690c53de2a1112c076f353bf13
pass0210 <pass0210@gmail.com>
2024-04-01 21:41:36 +0900

    Refactor: LogoutController 수정
    accessCookie와 refreshCookie가 null일경우 로직 변경

--- src/main/java/com/nhnacademy/front/controller/LogoutController.java
+++ src/main/java/com/nhnacademy/front/controller/LogoutController.java
@@ -23,28 +23,30 @@ public class LogoutController {
     @GetMapping("/logout")
     public String logout(HttpServletRequest request, HttpServletResponse response, @RequestAttribute("_csrf") CsrfToken csrfToken) {
         Cookie accessCookie = Arrays.stream(request.getCookies())
-                                    .filter(c -> "accessToken".equals(c.getName()))
-                                    .findFirst()
-                                    .orElse(new Cookie("accessToken", ""));
+                .filter(c -> "accessToken".equals(c.getName()))
+                .findFirst()
+                .orElse(null);
         Cookie refreshCookie = Arrays.stream(request.getCookies())
-                                     .filter(c -> "refreshToken".equals(c.getName()))
-                                     .findFirst()
-                                     .orElse(new Cookie("refreshToken", ""));
+                .filter(c -> "refreshToken".equals(c.getName()))
+                .findFirst()
+                .orElse(null);
 
-        accessCookie.setMaxAge(0);
-        accessCookie.setPath("/");
-        accessCookie.setHttpOnly(true);
-        response.addCookie(accessCookie);
-        refreshCookie.setMaxAge(0);
-        refreshCookie.setPath("/");
-        refreshCookie.setHttpOnly(true);
-        response.addCookie(refreshCookie);
 
-        if (Objects.nonNull(accessCookie.getValue()) && !"".equals(accessCookie.getValue())) {
+        if (Objects.nonNull(accessCookie) && !"".equals(accessCookie.getValue())) {
+            accessCookie.setMaxAge(0);
+            accessCookie.setPath("/");
+            accessCookie.setHttpOnly(true);
+            response.addCookie(accessCookie);
             redisUtil.setBlackList(accessCookie.getValue(), "access_token", (long) (1000 * 60 * 60));
         }
 
-        userAdapter.doLogout(refreshCookie.getValue(), csrfToken.getToken());
+        if (Objects.nonNull(refreshCookie) && !"".equals(refreshCookie.getValue())) {
+            refreshCookie.setMaxAge(0);
+            refreshCookie.setPath("/");
+            refreshCookie.setHttpOnly(true);
+            response.addCookie(refreshCookie);
+            userAdapter.doLogout(refreshCookie.getValue(), csrfToken.getToken());
+        }
 
         return "redirect:/";
     }
