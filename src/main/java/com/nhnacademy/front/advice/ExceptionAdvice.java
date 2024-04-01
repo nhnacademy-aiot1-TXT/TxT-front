f4cfb61326f32fc9a2b75b2acec820a6e5597c61
pass0210 <pass0210@gmail.com>
2024-04-01 21:55:45 +0900

    Refactor: ExceptionAdvice 수정

--- src/main/java/com/nhnacademy/front/advice/ExceptionAdvice.java
+++ src/main/java/com/nhnacademy/front/advice/ExceptionAdvice.java
@@ -11,23 +11,27 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
+import java.util.Objects;
 
 @ControllerAdvice(basePackages = "com.nhnacademy.front")
 @RequiredArgsConstructor
 public class ExceptionAdvice {
     private final UserAdapter userAdapter;
+
     @ExceptionHandler(FeignException.class)
     public String feignExceptionHandler(FeignException exception, HttpServletRequest request, HttpServletResponse response) {
         if (exception.status() == 401) {
             try {
                 Cookie refreshToken = Arrays.stream(request.getCookies()).filter(cookie -> cookie.getName().equals("refreshToken")).findFirst().orElse(null);
 
-                AccessTokenResponse accesstokenresponse = userAdapter.reissue(refreshToken.getValue());
-                Cookie accessCookie = new Cookie("accessToken", accesstokenresponse.getAccessToken());
-                accessCookie.setHttpOnly(true);
-                accessCookie.setPath("/");
+                if (Objects.nonNull(refreshToken) && !"".equals(refreshToken.getValue())) {
+                    AccessTokenResponse accesstokenresponse = userAdapter.reissue(refreshToken.getValue());
+                    Cookie accessCookie = new Cookie("accessToken", accesstokenresponse.getAccessToken());
+                    accessCookie.setHttpOnly(true);
+                    accessCookie.setPath("/");
 
-                response.addCookie(accessCookie);
+                    response.addCookie(accessCookie);
+                }
                 return "redirect:" + request.getRequestURI();
             } catch (FeignException e) {
                 return "redirect:/logout";
