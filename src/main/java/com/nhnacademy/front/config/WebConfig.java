4b391809b362688d8c7afa09d419b0a234f5e60e
woooyas <njmin8@naver.com>
2024-04-04 16:18:13 +0900

    Feat: model에 권한 속성을 추가해주는 기능을 aop에서 interceptor로 변경

--- src/main/java/com/nhnacademy/front/config/WebConfig.java
+++ src/main/java/com/nhnacademy/front/config/WebConfig.java
@@ -1,25 +1,37 @@
 package com.nhnacademy.front.config;
 
+import com.nhnacademy.front.interceptor.AddAuthorityToModelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.Duration;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final AddAuthorityToModelInterceptor addAuthorityToModelInterceptor;
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
     }
+
     @Bean
     RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder
                 .setReadTimeout(Duration.ofMillis(30000))
                 .setConnectTimeout(Duration.ofMillis(30000))
                 .build();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(addAuthorityToModelInterceptor);
     }
 }
