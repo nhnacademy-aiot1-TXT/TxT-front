caaa1c3e92f9a153b3ac200a1cd8529009ef9125
GGilook <rkfaorl1480@gmail.com>
2024-04-22 16:57:19 +0900

    Test: 테스트 코드 수정 및 추가.

--- src/test/java/com/nhnacademy/front/controller/UserControllerTest.java
+++ src/test/java/com/nhnacademy/front/controller/UserControllerTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.nhnacademy.front.adaptor.UserAdapter;
 import com.nhnacademy.front.dto.UserDataResponse;
 import com.nhnacademy.front.dto.UserRegisterRequest;
+import com.nhnacademy.front.dto.UserUpdateRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
@@ -19,8 +21,7 @@ import javax.servlet.http.Cookie;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class UserControllerTest {
     @Mock
@@ -58,8 +59,9 @@ class UserControllerTest {
         Model model = new ExtendedModelMap();
         RuntimeException testException = new RuntimeException("{\"title\":\"Test Error Message\"}");
 
-        // When
         doThrow(testException).when(userAdapter).createUser(any(UserRegisterRequest.class), any(String.class));
+
+        // When
         String result = userController.register(userRegisterRequest, new HttpSessionCsrfTokenRepository().generateToken(null), model);
 
         // Then
@@ -86,9 +88,64 @@ class UserControllerTest {
         String result = userController.profile(request, model);
 
         // Then
-        assertEquals("profile", result);
         UserDataResponse user = (UserDataResponse) model.getAttribute("user");
+
+        assertEquals("profile", result);
         assertEquals("testUser", user.getId());
         assertEquals("test@example.com", user.getEmail());
+    }
+
+    @Test
+    void update() throws JsonProcessingException {
+        // Given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Model model = new ExtendedModelMap();
+
+        // mock accessToken 쿠키
+        Cookie accessTokenCookie = new Cookie("accessToken", "testAccessToken");
+        request.setCookies(accessTokenCookie);
+
+        UserDataResponse mockUserDataResponse = new UserDataResponse();
+        mockUserDataResponse.setId("testUser");
+        mockUserDataResponse.setEmail("test@example.com");
+
+        when(userAdapter.getUserData(anyString())).thenReturn(mockUserDataResponse);
+
+        // When
+        String result = userController.update(request, response, model);
+
+        //Then
+        UserDataResponse user = (UserDataResponse) model.getAttribute("user");
+
+        assertEquals("redirect:/user/profile", result);
+        assertEquals("testUser", user.getId());
+        assertEquals("test@example.com", user.getEmail());
+        assertEquals("isUpdateSuccess", response.getCookie("isUpdateSuccess").getName());
+        assertEquals("success", response.getCookie("isUpdateSuccess").getValue());
+        verify(userAdapter, times(1)).updateUser(any(UserUpdateRequest.class), anyString());
+    }
+
+    @Test
+    void update_exception() throws JsonProcessingException {
+        // Given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Model model = new ExtendedModelMap();
+        RuntimeException testException = new RuntimeException("{\"title\":\"Test Error Message\"}");
+
+        // mock accessToken 쿠키
+        Cookie accessTokenCookie = new Cookie("accessToken", "testAccessToken");
+        request.setCookies(accessTokenCookie);
+
+        when(userAdapter.getUserData(anyString())).thenReturn(new UserDataResponse());
+        doThrow(testException).when(userAdapter).updateUser(any(UserUpdateRequest.class), anyString());
+
+        // When
+        String result = userController.update(request, response, model);
+
+        // Then
+        assertEquals("profile", result);
+
     }
 }
