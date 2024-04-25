c1a3a4e15e0144270b6ed3e06c6344b653383d50
GGilook <rkfaorl1480@gmail.com>
2024-04-25 15:41:48 +0900

    Feat: Spring Security 의 세션 저장을 위한 multiple redis database 설정.

--- src/main/java/com/nhnacademy/front/config/RedisConfig.java
+++ src/main/java/com/nhnacademy/front/config/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.nhnacademy.front.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -17,10 +19,40 @@ public class RedisConfig {
      * @param redisConnectionFactory Redis 연결 팩토리
      * @return RedisTemplate 객체
      */
+
+    @Value("${spring.redis.database}")
+    private int database;
+
+    @Value("${spring.redis.host}")
+    private String name;
+
+    @Value("${spring.redis.blacklist.password}")
+    private String password;
+
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplateSpringSecurity(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    public RedisConnectionFactory redisConnectionFactoryBlackList() {
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setDatabase(database);
+        lettuceConnectionFactory.setHostName(name);
+        lettuceConnectionFactory.afterPropertiesSet();
+        lettuceConnectionFactory.setPassword(password);
+        return lettuceConnectionFactory;
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplateBlackList() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactoryBlackList());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
