06b30632b95eb3cc51c2c0430982cab3f0d82110
GGilook <rkfaorl1480@gmail.com>
2024-04-25 16:32:27 +0900

    Fix: redis bean 설정 바꾸기 (blacklist db : 10, security db : 18).

--- src/main/java/com/nhnacademy/front/config/RedisConfig.java
+++ src/main/java/com/nhnacademy/front/config/RedisConfig.java
@@ -20,17 +20,17 @@ public class RedisConfig {
      * @return RedisTemplate 객체
      */
 
-    @Value("${spring.redis.database}")
+    @Value("${spring.redis.host}")
+    private String hostname;
+
+    @Value("${spring.redis.security.database}")
     private int database;
 
-    @Value("${spring.redis.host}")
-    private String name;
-
-    @Value("${spring.redis.blacklist.password}")
+    @Value("${spring.redis.security.password}")
     private String password;
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplateSpringSecurity(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -41,18 +41,18 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    public RedisConnectionFactory redisConnectionFactoryBlackList() {
+    public RedisConnectionFactory redisConnectionFactorySecurity() {
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName(hostname);
         lettuceConnectionFactory.setDatabase(database);
-        lettuceConnectionFactory.setHostName(name);
         lettuceConnectionFactory.afterPropertiesSet();
         lettuceConnectionFactory.setPassword(password);
         return lettuceConnectionFactory;
     }
     @Bean
-    public RedisTemplate<String, Object> redisTemplateBlackList() {
+    public RedisTemplate<String, Object> redisTemplateSecurity() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactoryBlackList());
+        redisTemplate.setConnectionFactory(redisConnectionFactorySecurity());
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
