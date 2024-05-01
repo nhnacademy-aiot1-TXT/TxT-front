f69af8e08a9dca146d7b8bc02a5e28b69d9af543
Eunsu <ujd017@naver.com>
2024-05-01 15:23:51 +0900

    변경

--- src/main/java/com/nhnacademy/front/controller/AdminController.java
+++ src/main/java/com/nhnacademy/front/controller/AdminController.java
@@ -1,17 +1,43 @@
 package com.nhnacademy.front.controller;
 
+import com.nhnacademy.front.dto.UserDataResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+import com.nhnacademy.front.adaptor.UserAdapter;
 
 /**
  * 어드민 권한만 접근할 수 있는 Controller
  */
 @RequestMapping("/admin")
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final UserAdapter userAdapter;
+
     @GetMapping
     public String admin() {
         return "redirect:/";
+    }
+
+    @GetMapping("/dtsensor")
+    public String profile(HttpServletRequest request, Model model) {
+        String accessToken = Arrays.stream(request.getCookies())
+                .filter(cookie -> "accessToken".equals(cookie.getName()))
+                .findFirst()
+                .orElse(null)
+                .getValue();
+
+        UserDataResponse user = userAdapter.getUserData(accessToken);
+        model.addAttribute("user", user);
+
+        return "detailedSensor";
     }
 }
