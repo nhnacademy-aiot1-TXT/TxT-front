40c699e16e8aef0678132092235ea794505e6aba
jongsikk <xxx_@kakao.com>
2024-05-16 21:13:17 +0900

    Feat: place 조회 추가 및 device 조회 수정

--- src/main/java/com/nhnacademy/front/adaptor/DeviceSettingAdapter.java
+++ src/main/java/com/nhnacademy/front/adaptor/DeviceSettingAdapter.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.front.adaptor;
 
+
 import com.nhnacademy.front.dto.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -8,8 +9,14 @@ import java.util.List;
 
 @FeignClient(value = "common-api", url = "${gateway.url}", path = "/api/common")
 public interface DeviceSettingAdapter {
+    @GetMapping("/place/{placeId}")
+    PlaceResponse getPlace(@RequestHeader("Authorization") String accessToken, @PathVariable Long placeId);
+
+    @GetMapping("/device/devices/{placeId}")
+    List<DeviceResponse> getDeviceListByPlace(@RequestHeader("Authorization") String accessToken, @PathVariable Long placeId);
+
     @GetMapping("/device")
-    DeviceResponse getDevice(@RequestHeader("Authorization") String accessToken, @RequestParam String name);
+    DeviceResponse getDevice(@RequestHeader("Authorization") String accessToken, @RequestParam String placeName, @RequestParam String name);
 
     @GetMapping("/device-sensor/{deviceId}")
     List<DeviceSensorResponse> getSensorList(@RequestHeader("Authorization") String accessToken, @PathVariable Long deviceId);
@@ -17,11 +24,17 @@ public interface DeviceSettingAdapter {
     @PutMapping("/device/{deviceId}")
     DeviceResponse updateDevice(@RequestHeader("Authorization") String accessToken, @PathVariable String deviceId, @RequestBody DeviceRequest deviceRequest);
 
+    @GetMapping("/device-sensor/{deviceId}/{sensorId}")
+    DeviceSensorResponse getDeviceSensorById(@RequestHeader("Authorization") String accessToken, @PathVariable Long deviceId, @PathVariable Long sensorId);
+
     @GetMapping("/device-sensor/sensor")
-    DeviceSensorResponse getDeviceSensor(@RequestHeader("Authorization") String accessToken, @RequestParam String deviceName, @RequestParam String sensorName);
+    DeviceSensorResponse getDeviceSensorByName(@RequestHeader("Authorization") String accessToken, @RequestParam String deviceName, @RequestParam String sensorName, @RequestParam String placeName);
 
     @PutMapping("/device-sensor/{deviceId}/{sensorId}")
     DeviceSensorResponse updateDeviceSensor(@RequestHeader("Authorization") String accessToken, @PathVariable Long deviceId, @PathVariable Long sensorId, @RequestBody DeviceSensorRequest deviceSensorRequest);
+
+    @PutMapping("/place/{placeId}")
+    PlaceResponse updatePlace(@RequestHeader("Authorization") String accessToken, @PathVariable Long placeId, @RequestBody PlaceRequest placeRequest);
 
     @GetMapping("/time-interval")
     TimeIntervalResponse getTimeInterval(@RequestHeader("Authorization") String accessToken, @RequestParam String sensorName);
