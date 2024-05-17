e0814d323a8755554a94b5cc1098141d8188d900
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-05-17 17:38:07 +0900

    Feature: 디바이스 추가 dto 매핑중

--- src/main/java/com/nhnacademy/front/dto/rule/CustomModeDto.java
+++ src/main/java/com/nhnacademy/front/dto/rule/CustomModeDto.java
@@ -5,15 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalTime;
 import java.util.Map;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class CustomMode {
+public class CustomModeDto {
     private Map<MqttInDto, ConditionSetDto> mqttConditionMap;
     private Integer hour;
     private Integer minutes;
 }
-
