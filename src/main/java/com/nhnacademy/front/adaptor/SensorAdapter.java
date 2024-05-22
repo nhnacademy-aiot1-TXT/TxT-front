d776604b4da161fd2c3b48035358176b99e4879b
서길우 <rkfaorl1480@gmail.com>
2024-05-22 12:26:08 +0900

    Refactor: 센서 데이터 DTO 수정.
    - html 구조 수정으로 인한 DTO 수정 및 삭제.
      - 센서 데이터 기준으로 나눈 페이지를 일/월/주, 온도/습도/조도/co2, 장소 정보를 form 에서 일괄적으로 입력하도록 만듦에 따라, 요구하는 값이 달라지기에 수정함.
      - @JsonAlias 를 이용하여 adaptor 로 받는 DTO 내의 변수가 달라도 한 DTO 에 매핑되도록 수정.
    - 그에따른 SensorAdapter 리턴값도 수정.
    - PlaceResponse 내 placeCode 변수 추가.

--- src/main/java/com/nhnacademy/front/adaptor/SensorAdapter.java
+++ src/main/java/com/nhnacademy/front/adaptor/SensorAdapter.java
@@ -1,8 +1,6 @@
 package com.nhnacademy.front.adaptor;
 
 import com.nhnacademy.front.dto.*;
-import com.nhnacademy.front.dto.IlluminationResponse.IlluminationResponse;
-import com.nhnacademy.front.page.RestPage;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +11,56 @@ import java.util.List;
  */
 @FeignClient(value = "sensor-data", url = "${gateway.url}")
 public interface SensorAdapter {
-
-
-    @GetMapping("/api/sensor/humidity/week")
-    List<HumidityResponse> getWeeklyHumidity(@RequestHeader("Authorization") String accessToken);
-
-    @GetMapping("/api/sensor/co2/week")
-    List<Co2Response> getWeeklyCo2(@RequestHeader("Authorization") String accessToken);
+    @GetMapping("/api/sensor/temperature/day")
+    List<DailySensorResponse> getDailyTemperatures(@RequestHeader("Authorization") String accessToken,
+                                                   @RequestParam String place);
 
     @GetMapping("/api/sensor/temperature/week")
-    List<TemperatureResponse> getWeeklyTemperatures(@RequestHeader("Authorization") String accessToken);
+    List<WeeklySensorResponse> getWeeklyTemperatures(@RequestHeader("Authorization") String accessToken,
+                                                    @RequestParam String place);
+
+    @GetMapping("/api/sensor/temperature/month")
+    List<MonthlySensorResponse> getMonthlyTemperatures(@RequestHeader("Authorization") String accessToken,
+                                                     @RequestParam String place);
+
+    @GetMapping("/api/sensor/illumination/day")
+    List<DailySensorResponse> getDailyIllumination(@RequestHeader("Authorization") String accessToken,
+                                                    @RequestParam String place);
 
     @GetMapping("/api/sensor/illumination/week")
-    List<IlluminationResponse> getWeeklyIllumination(@RequestHeader("Authorization") String accessToken);
+    List<WeeklySensorResponse> getWeeklyIllumination(@RequestHeader("Authorization") String accessToken,
+                                                     @RequestParam String place);
+
+    @GetMapping("/api/sensor/illumination/month")
+    List<MonthlySensorResponse> getMonthlyIllumination(@RequestHeader("Authorization") String accessToken,
+                                                      @RequestParam String place);
+
+    @GetMapping("/api/sensor/humidity/day")
+    List<DailySensorResponse> getDailyHumidity(@RequestHeader("Authorization") String accessToken,
+                                            @RequestParam String place);
+
+    @GetMapping("/api/sensor/humidity/week")
+    List<WeeklySensorResponse> getWeeklyHumidity(@RequestHeader("Authorization") String accessToken,
+                                             @RequestParam String place);
+
+
+    @GetMapping("/api/sensor/humidity/month")
+    List<MonthlySensorResponse> getMonthlyHumidity(@RequestHeader("Authorization") String accessToken,
+                                              @RequestParam String place);
+
+    @GetMapping("/api/sensor/co2/day")
+    List<DailySensorResponse> getDailyCo2(@RequestHeader("Authorization") String accessToken,
+                                  @RequestParam String place);
+
+
+    @GetMapping("/api/sensor/co2/week")
+    List<WeeklySensorResponse> getWeeklyCo2(@RequestHeader("Authorization") String accessToken,
+                                   @RequestParam String place);
+
+
+    @GetMapping("/api/sensor/co2/month")
+    List<MonthlySensorResponse> getMonthlyCo2(@RequestHeader("Authorization") String accessToken,
+                                    @RequestParam String place);
 
 
 }
