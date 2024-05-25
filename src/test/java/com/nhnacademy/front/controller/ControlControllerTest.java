0b3b095239ae78db635b7c9c7ef28b968cf9b624
pass0210 <pass0210@gmail.com>
2024-05-25 15:28:46 +0900

    Refactor: ControlControllerTest 수정
    ai 결과 api test 추가

--- src/test/java/com/nhnacademy/front/controller/ControlControllerTest.java
+++ src/test/java/com/nhnacademy/front/controller/ControlControllerTest.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -40,8 +41,8 @@ class ControlControllerTest {
     private HttpServletRequest request;
 
     private static final String ACCESS_TOKEN = "test token";
-
     private static final String DEVICE_KEY = "device_power_status";
+    private static final String PREDICT_DATA = "predict_data";
 
     @BeforeEach
     void setUp() throws Exception {
@@ -96,5 +97,38 @@ class ControlControllerTest {
         boolean isOn = true;
 
         mockMvc.perform(get("/control/custom-mode").param("deviceName", deviceName).param("placeCode", place).param("isOn", Boolean.toString(isOn))).andDo(print()).andExpect(status().is3xxRedirection()).andExpect(handler().handlerType(ControlController.class)).andExpect(handler().methodName("customMode")).andExpect(redirectedUrl("/control"));
+    }
+
+    @Test
+    void getAiResult() throws Exception {
+        String deviceName = "test device";
+        String time = "X시 X분";
+        String indoorTemperature = "23";
+        String indoorHumidity = "50";
+        String outdoorTemperature = "27";
+        String outdoorHumidity = "54";
+        String totalPeopleCount = "9";
+        String result = "ON";
+
+        given(redisUtil.getAiInfo(PREDICT_DATA, "deviceName")).willReturn(deviceName);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "time")).willReturn(time);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "indoorTemperature")).willReturn(indoorTemperature);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "indoorHumidity")).willReturn(indoorHumidity);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "outdoorTemperature")).willReturn(outdoorTemperature);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "outdoorHumidity")).willReturn(outdoorHumidity);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "totalPeopleCount")).willReturn(totalPeopleCount);
+        given(redisUtil.getAiInfo(PREDICT_DATA, "result")).willReturn(result);
+
+        mockMvc.perform(get("/control/ai-result"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$['deviceName']", equalTo(deviceName)))
+                .andExpect(jsonPath("$['time']", equalTo(time)))
+                .andExpect(jsonPath("$['indoorTemperature']", equalTo(indoorTemperature)))
+                .andExpect(jsonPath("$['indoorHumidity']", equalTo(indoorHumidity)))
+                .andExpect(jsonPath("$['outdoorTemperature']", equalTo(outdoorTemperature)))
+                .andExpect(jsonPath("$['outdoorHumidity']", equalTo(outdoorHumidity)))
+                .andExpect(jsonPath("$['totalPeopleCount']", equalTo(totalPeopleCount)))
+                .andExpect(jsonPath("$['result']", equalTo(result)));
     }
 }
