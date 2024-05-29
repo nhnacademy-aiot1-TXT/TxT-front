261838fe1e3c14a946b7e3dd56f233bb9a157039
jjunho50 <jjunho50@gmail.com>
2024-05-29 11:28:23 +0900

    Fix : 하드코딩 되어있던 deviceName, placeName을 thymeleaf로 변경

--- src/main/java/com/nhnacademy/front/controller/RuleRegisterController.java
+++ src/main/java/com/nhnacademy/front/controller/RuleRegisterController.java
@@ -1,32 +1,33 @@
 package com.nhnacademy.front.controller;
 
-import com.nhnacademy.front.adaptor.RuleRegisterAdaptor;
-import com.nhnacademy.front.error.DeviceRegisterException;
+import com.nhnacademy.front.adaptor.DeviceSettingAdapter;
+import com.nhnacademy.front.dto.DeviceResponse;
+import com.nhnacademy.front.dto.PlaceResponse;
 import com.nhnacademy.front.utils.AccessTokenUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/admin/rule")
 @RequiredArgsConstructor
 public class RuleRegisterController {
+    private final DeviceSettingAdapter deviceSettingAdapter;
 
-    private final RuleRegisterAdaptor ruleRegisterAdaptor;
-
-    @PostMapping("/send-data")
-    public ResponseEntity<String> getRuleRegisterInfo(HttpServletRequest request, @RequestBody String ruleRegisterInfo) {
+    @GetMapping("/register")
+    public String ruleRegister(HttpServletRequest request, Model model) {
         String accessToken = AccessTokenUtil.findAccessTokenInRequest(request);
-        try {
-            ruleRegisterAdaptor.sendRuleRegisterInfo(accessToken, ruleRegisterInfo);
-            return ResponseEntity.ok("Rule registered successfully!");
-        } catch (Exception e) {
-            throw new DeviceRegisterException("Rule registered failed!");
-        }
+        List<PlaceResponse> placeList = deviceSettingAdapter.getPlaceList(accessToken);
+        List<DeviceResponse> deviceList = deviceSettingAdapter.getDeviceListByPlace(accessToken, 1L);
+
+        model.addAttribute("deviceList", deviceList);
+        model.addAttribute("placeList", placeList);
+
+        return "rule-register";
     }
 }
