4389e41cb6ae0fc839ee41e882372d410d8b0a2c
pass0210 <pass0210@gmail.com>
2024-06-01 15:28:49 +0900

    Refactor: UserRegisterRequest 수정
    Validation annotation 추가

--- src/main/java/com/nhnacademy/front/dto/UserRegisterRequest.java
+++ src/main/java/com/nhnacademy/front/dto/UserRegisterRequest.java
@@ -3,12 +3,19 @@ package com.nhnacademy.front.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class UserRegisterRequest {
+    @NotBlank(message = "아이디가 입력되지 않았습니다.")
     private String id;
+    @NotBlank(message = "이름이 입력되지 않았습니다.")
     private String name;
+    @NotBlank(message = "비밀번호가 입력되지 않았습니다.")
     private String password;
+    @NotBlank(message = "이메일이 입력되지 않았습니다.")
     private String email;
+    @NotBlank(message = "생일이 입력되지 않았습니다.")
     private String birth;
 }
