cd33c9af7a5cb15d7ecd6a25be043f5899083ef5
서길우 <rkfaorl1480@gmail.com>
2024-06-04 10:12:01 +0900

    Chore: 사소한 수정.
    - notification 기능 www 붙을 시 cors 걸리는 링크 수정.
    - 필요없는 js 파일 하나 삭제.

--- src/main/resources/static/js/notification.js
+++ src/main/resources/static/js/notification.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 }
 
             }
-            const response = await fetch('https://contxt.co.kr/api/common/notification', option);
+            const response = await fetch('/api/common/notification', option);
 
             if (!response.ok) {
                 throw new Error('Network response was not ok');
